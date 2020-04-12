f6d4e9b5272992f9b470499ec39d6aac858584c8
ashraful <ashsaas.islam@gmail.com>
2020-04-11 20:53:18 -0400

    Adding/removing friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,5 +54,7 @@ def self.search(param)
   def except_current_user(users)
 users.reject{|user| user.id==self.id}
   end
-
+  def not_friends_with?(id_of_friend)
+    !self.friends.where(id:id_of_friend).exists?
+end
 end
